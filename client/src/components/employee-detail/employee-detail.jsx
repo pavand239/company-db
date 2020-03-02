6ab740813766f2bf03d88ac274fc074c6b3349c6
Pavlov Andrey <pavand239@gmail.com>
2020-03-02 17:26:35 +0300

    Add employee-detail for different groups

--- client/src/components/employee-detail/employee-detail.jsx
+++ client/src/components/employee-detail/employee-detail.jsx
@@ -1,5 +1,5 @@
 import React, {useCallback} from 'react';
-
+import {connect} from 'react-redux';
 
 import { withCompanyDBService } from '../hoc';
 import {useGetData} from "../hooks";
@@ -9,7 +9,16 @@ import EmployeeEducationTable from '../empoyee-education-table';
 import ErrorIndicator from "../error-indicator";
 import LoadingIndicator from "../loading-indicator";
 
-const EmployeeDetail = ({employeeId, companyDBService}) => {
+// Possible data fields
+// {surname, name, patronymic, birth_date, birth_place, 
+//     department, position, passport_series, passport_ID,
+//     address, salary, attitude_to_conscription:attToCons, 
+//     marital_status:maritalStatus, gender}
+// Possible user groups
+//['Admin','Chief','Accounting','HumanResource','Union']
+
+
+const EmployeeDetail = ({employeeId, companyDBService, user:{user:{groups}}}) => {
     const useGetEmployee = () => {
         let token = localStorage.getItem('token'),
             getEmployee = useCallback(()=>companyDBService.getEmployee(token,employeeId),[employeeId, token]);
@@ -23,22 +32,19 @@ const EmployeeDetail = ({employeeId, companyDBService}) => {
     if (error) {
         return  error.message
     }
-    let {surname, name, patronymic, birth_date, birth_place, 
-        department, position, passport_series, passport_ID,
-        address, salary, attitude_to_conscription:attToCons, 
-        marital_status:maritalStatus, gender} = data;
+    let moreDetail = '';
+
+    if (groups.includes('Chief') || 
+        groups.includes('Accounting') || 
+        groups.includes('HumanResource')) {
+            moreDetail = <EmployeeDetailChief data={data} />
+    } else if (groups.includes('Admin')) {
+        moreDetail = <EmployeeDetailAdmin data={data} />
+    }
     return (
         <div>
-            <h3>{surname} {name} {patronymic}</h3>
-            <p>Родился в {birth_date}, {birth_place}</p>
-            <p>Отдел: {department}</p>
-            <p>Должность: {position}</p>
-            <p>Оклад: {salary} руб.</p>
-            <p>Пол: {gender}</p>
-            <p>Паспорт: {passport_series} {passport_ID}</p>
-            <p>Проживает: {address}</p>
-            <p>Семейное положение: {maritalStatus}</p>
-            <p>Отношение к воинской обязанности: {attToCons}</p>
+            <EmployeeDetailDefault data={data} />
+            {moreDetail}
             <p>Доходы:</p>
             <div className='ml-3 my-3'>
                 <EmployeeIncomeTable employeeId={employeeId}/>
@@ -55,22 +61,47 @@ const EmployeeDetail = ({employeeId, companyDBService}) => {
         </div>
     )
 }
-const EmployeeDetailDefault = (data) => (
-    <div>
-        <h3>{data.surname} {data.name} {data.patronymic}</h3>
-        <p>Родился в {data.birth_date}, {data.birth_place}</p>
-        <p>Отдел: {data.department}</p>
-        <p>Должность: {data.position}</p>
-        <p>Пол: {data.sex}</p>
-    </div>
-)
-const EmployeeDetailChief=(data,childrenData, incomeData, educationData) => (
-    <div>
-        <p>Оклад: {data.salary} руб.</p>
-        <p>Паспорт: {data.passport_series} {data.passport_ID}</p>
-        <p>Проживает: {data.address}</p>
-        <p>Семейное положение: {data.maritalStatus}</p>
-        <p>Отношение к воинской обязанности: {data.attToCons}</p>
-    </div>
-)
-export default withCompanyDBService(EmployeeDetail);
+const EmployeeDetailDefault = ({data}) => {
+    let {surname, name, patronymic, birth_date, birth_place, 
+        department, position, gender} = data;
+    return (
+        <div>
+            <h3>{surname} {name} {patronymic}</h3>
+            <p>Родился в {birth_date}, {birth_place}</p>
+            <p>Отдел: {department}</p>
+            <p>Должность: {position}</p>
+            <p>Пол: {gender}</p>
+        </div>
+    )
+}
+const EmployeeDetailAdmin=({data}) => {
+    let {address, salary, attitude_to_conscription:attToCons, 
+        marital_status:maritalStatus} = data;
+    return (
+        <div>
+            <p>Оклад: {salary} руб.</p>
+            <p>Проживает: {address}</p>
+            <p>Семейное положение: {maritalStatus}</p>
+            <p>Отношение к воинской обязанности: {attToCons}</p>
+        </div>
+    )
+}
+const EmployeeDetailChief=({data}) => {
+    let {passport_series, passport_ID,
+        address, salary, attitude_to_conscription:attToCons, 
+        marital_status:maritalStatus} = data;
+    return (
+        <div>
+            <p>Оклад: {salary} руб.</p>
+            <p>Паспорт: {passport_series} {passport_ID}</p>
+            <p>Проживает: {address}</p>
+            <p>Семейное положение: {maritalStatus}</p>
+            <p>Отношение к воинской обязанности: {attToCons}</p>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    user:state.user
+})
+export default connect(mapStateToProps)(withCompanyDBService(EmployeeDetail));
