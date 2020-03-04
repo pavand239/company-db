ece54903db045c9fa0866dcfd787cd124dde5264
Pavlov Andrey <pavand239@gmail.com>
2020-03-04 18:33:01 +0300

    Add all fields for Employee model

--- client/src/components/form-configs/form-fields.js
+++ client/src/components/form-configs/form-fields.js
@@ -16,6 +16,19 @@ export const patronymicField = (readOnly=true)=> ({
     readOnly: readOnly,
     type:'text'
 });
+export const birthDateField = (readOnly=true) => ({
+    name:'birth_date',
+    label:'Дата рождения',
+    readOnly: readOnly,
+    type:'date'
+});
+export const birthPlaceField = (readOnly=true) => ({
+    name:'birth_place',
+    label:'Место рождения',
+    readOnly: readOnly,
+    type:'text'
+});
+
 export const departmentField =(readOnly=true)=> ({
     name:'department',
     label:'Отдел',
@@ -27,4 +40,52 @@ export const positionField = (readOnly=true) => ({
     label:'Должность',
     readOnly: readOnly,
     type:'text'
+});
+export const passportSeriesField = (readOnly=true) => ({
+    name:'passport_series',
+    label:'Серия паспорта',
+    readOnly: readOnly,
+    type:'text'
+});
+export const passportIDField = (readOnly=true) => ({
+    name:'passport_ID',
+    label:'Номер пасспорта',
+    readOnly: readOnly,
+    type:'text'
+});
+export const addressField = (readOnly=true) => ({
+    name:'address',
+    label:'Адрес',
+    readOnly: readOnly,
+    type:'text'
+});
+export const salaryField = (readOnly=true) => ({
+    name:'salary',
+    label:'Оклад',
+    readOnly: readOnly,
+    type:'number'
+});
+export const attToConsField = (readOnly=true) => ({
+    name:'attitude_to_conscription',
+    label:'Отношение к воинской обязанности',
+    readOnly: readOnly,
+    element:'select',
+    options:['Негативное','Нейтральное','Позитивное'],
+    optionsValues:['neg','neu','pos']
+});
+export const maritalStatusField = (readOnly=true) => ({
+    name:'marital_status',
+    label:'Семейное положение',
+    readOnly: readOnly,
+    element:'select',
+    options:['Не женат/не замужем','Женат/замужем'],
+    optionsValues:['y','n']
+});
+export const genderField = (readOnly=true) => ({
+    name:'gender',
+    label:'Пол',
+    readOnly: readOnly,
+    element:'select',
+    options:['Мужской', 'Женский'],
+    optionsValues:['m','f']
 });
