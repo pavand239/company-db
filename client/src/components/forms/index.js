47220908e7f6bbcb5478e88d5bb1187458befd3f
Pavlov Andrey <pavand239@gmail.com>
2020-03-05 17:24:55 +0300

    add income form

--- client/src/components/forms/index.js
+++ client/src/components/forms/index.js
@@ -3,9 +3,12 @@ import {
     EmployeeEditAccounting,
     EmployeeEditHumanResource
 } from "./employee-edit";
-
+import {
+    IncomeEditChief
+} from "./income-form"
 export {
     EmployeeEditChief,
     EmployeeEditAccounting,
-    EmployeeEditHumanResource
+    EmployeeEditHumanResource,
+    IncomeEditChief
 }
