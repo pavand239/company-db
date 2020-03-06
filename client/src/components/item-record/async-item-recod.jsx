ceb4c20b28f3ccfe00e45f126c549d25a982ff65
Pavlov Andrey <pavand239@gmail.com>
2020-03-06 11:23:32 +0300

    add item record to view async data

---
+++ client/src/components/item-record/async-item-recod.jsx
@@ -0,0 +1,22 @@
+import React, { useCallback} from 'react';
+import LoadingIndicator from "../loading-indicator";
+import {useGetData} from "../hooks";
+import ManyFieldItemRecord from "./many-field-item-record"
+
+const AsyncItemRecord = ({getData, field, item, fieldToDisplay, label=null})=>{
+    const useGetDataCallback = () => {
+        let token = localStorage.getItem('token'),
+            getDataCallback = useCallback(()=>getData(token, item[field]),[token]);
+        return useGetData(getDataCallback);
+    }
+    let {data, error, isLoading} = useGetDataCallback();
+    console.log(label)
+    if (!error && isLoading){
+        return <LoadingIndicator />
+    }
+    if (error){
+        return error.message
+    }
+    return <ManyFieldItemRecord item={data} fieldToDisplay={fieldToDisplay} label={label} />
+}
+export default AsyncItemRecord;
