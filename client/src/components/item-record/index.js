752be421b9a89aac105989651bf66712db102f1c
Pavlov Andrey <pavand239@gmail.com>
2020-03-06 11:23:53 +0300

    add ManyFiel and Async ItemRecord

--- client/src/components/item-record/index.js
+++ client/src/components/item-record/index.js
@@ -1,2 +1,9 @@
 import ItemRecord from "./item-record";
+import AsyncItemRecord from "./async-item-recod";
+import ManyFieldItemRecord from "./many-field-item-record";
+
 export default ItemRecord;
+export {
+    AsyncItemRecord,
+    ManyFieldItemRecord
+}
