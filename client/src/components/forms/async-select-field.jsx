3b34c13d6305a3213f8858399d058ddb6f3a4cab
Pavlov Andrey <pavand239@gmail.com>
2020-03-06 11:22:41 +0300

    small refactoring

--- client/src/components/forms/async-select-field.jsx
+++ client/src/components/forms/async-select-field.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, {useCallback} from 'react';
 import {Form} from "react-bootstrap";
 import LoadingIndicator from "../loading-indicator";
 import {useGetData} from "../hooks";
