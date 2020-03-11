24030cbf524b734727420d29d7c248e65ba139d3
Pavlov Andrey <pavand239@gmail.com>
2020-03-11 09:42:12 +0300

    add child edit config, employee admin, rename edu

--- client/src/components/form-configs/index.js
+++ client/src/components/form-configs/index.js
@@ -1,18 +1,22 @@
 import {
     EmployeeEditChiefConfig,
     EmployeeEditAccountingConfig,
-    EmployeeEditHumanResourceConfig
+    EmployeeEditHumanResourceConfig,
+    EmployeeEditAdminConfig
 } from "./employee-edit-configs";
 import {
     IncomeEditChiefConfig,
     IncomeEditAccountingConfig
 } from "./income-edit-configs"
-import { EducationEditForm } from "./education-edit-configs";
+import { EducationEditConfig } from "./education-edit-configs";
+import { ChildEditConfig } from "./child-edit-configs";
 export {
     EmployeeEditChiefConfig,
     EmployeeEditAccountingConfig,
     EmployeeEditHumanResourceConfig,
+    EmployeeEditAdminConfig,
     IncomeEditChiefConfig,
     IncomeEditAccountingConfig,
-    EducationEditForm
+    EducationEditConfig,
+    ChildEditConfig
 }
