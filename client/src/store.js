2fdd24a43b18c646801453667f98b81131bb21aa
Pavlov Andrey <pavand239@gmail.com>
2020-03-12 12:23:02 +0300

    add redeux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware} from "redux";
 import {reducer} from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(reducer,applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
